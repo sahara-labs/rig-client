128cde3e8b1ad6c47c52584b96b7490835df5ada
mdiponio <mdiponio@7ee224fa-57ca-4419-87f9-1f4314938d77>
2009-10-27 23:13:49 +0000

    Some AbstractBatchController implementation.
    git-svn-id: https://develop.eng.uts.edu.au/projects/personal/sahara/svn/RigClient/trunk@19 7ee224fa-57ca-4419-87f9-1f4314938d77

--- src/au/edu/uts/eng/remotelabs/rigclient/rig/control/AbstractBatchRunner.java
+++ src/au/edu/uts/eng/remotelabs/rigclient/rig/control/AbstractBatchRunner.java
@@ -41,6 +41,7 @@
  */
 package au.edu.uts.eng.remotelabs.rigclient.rig.control;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -57,7 +58,8 @@ import au.edu.uts.eng.remotelabs.rigclient.util.LoggerFactory;
  * <ul>
  *     <li><code>testFile()</code> - Should provide a sanity check
  *     on the provided batch instruction file.</li>
- *     <li>invoke() - Should start the batch control executable.</li>
+ *     <li>init() - Should setup the command, arguments, working directory and 
+ *     execution environment.</li>
  *     <li>sync() - This is run after the completion of batch
  *     control and should do synchronisation of the generated results 
  *     files to a persistent store.</li>
@@ -77,12 +79,18 @@ public abstract class AbstractBatchRunner implements Runnable
     /** File name of instruction file. */
     protected String fileName;
     
-    /** Processes working directory. */
+    /** Batch process working directory base (base has a new folder added to
+     *  be set as the batch processes CWD. */
+    private String workingDirBase;
+    
+    /** Batch process working directory. */
     private String workingDir;
     
     /** Enviroment variables for the batch process. */
-    private Map<String, String> env;
+    private Map<String, String> envMap;
     
+    /** Flag to specify if batch execution is running. */
+    private boolean isRunning;
     
     /** Logger. */
     protected ILogger logger;
@@ -94,7 +102,7 @@ public abstract class AbstractBatchRunner implements Runnable
         
         this.fileName = file;
         this.commandArgs = new ArrayList<String>();
-        this.env = new HashMap<String, String>();
+        this.envMap = new HashMap<String, String>();
     }
     
     public void run()
@@ -110,14 +118,22 @@ public abstract class AbstractBatchRunner implements Runnable
      *     <li><strong><code>command</code></strong> - The batch command name
      *     executable name.</li>
      *     <li><strong><code>commandArgs</code></strong> - The command line 
-     *     arguments to provide for the </li>
-     *     <li></li> 
+     *     arguments to provide for the command. The arguments are provided
+     *     in the same order as they are entered into the
+     *     <code>commandArgs</code> list. <strong>NOTE:</strong> You should
+     *     add the <code>fileName</code> field as a parameter for the command
+     *     since this contains an absolute reference to the uploaded 
+     *     instruction file.</li>
+     *     <li><strong><code>workingDirBase</code></strong> - The working
+     *     directory base for the batch command process. If not set, the default
+     *     base is the system temporary directory. The base is appended with
+     *     a new folder to contain the results files from the batch executable
+     *     (<em>assuming</em> it puts the files in its current working 
+     *     directory.)</li>
+     *     <li><strong><code>envMap</code></strong> - Environment variable map
+     *     for the batch process enviroment. Additions to this will be appended
+     *     to the default enviroment variables.</li>
      * </ul>
-     * 
-     * The arguments are provided to command line program in the order they
-     * are inserted into the <code>commandArgs</code> array. <strong>NOTE: </strong>
-     * You must add the <code>fileName</code> parameter at its appropriate 
-     * location in the argument sequence.
      * 
      * @return true if successful, false otherwise
      */
@@ -136,18 +152,43 @@ public abstract class AbstractBatchRunner implements Runnable
      */
     protected abstract boolean testFile();
     
-    
-    
     /**
      * Invokes the batch control executable providing the instruction files
-     * as a command line parameter. The function should return after the 
-     * invocation of the executable and not wait for it to be completed.
+     * as a command line parameter. Returns are invocation and does not
+     * wait for completeion.
      * 
      * @return true if the executable was successfully invoked
      */
-    protected boolean invoke()
+    public boolean invoke()
     {
+        List<String> invocation = new ArrayList<String>();
+        invocation.add(this.command);
+        invocation.addAll(this.commandArgs);
+        this.logger.info("Batch command that will be invoked is " + this.command);
+        this.logger.info("Batch command arguments are " + this.commandArgs.toString());
         
+        ProcessBuilder build = new ProcessBuilder(invocation);
+        
+        /* Working directory. */
+        if (this.workingDirBase != null)
+        {
+            this.logger.info("User set batch working directory is " + this.workingDirBase);
+            build.directory(new File(this.workingDirBase));
+        }
+        else
+        {
+            /* Default is a directory in the system directory. */
+            this.workingDirBase = System.getProperty("java.io.tmpdir");
+            if (this.workingDirBase == null)
+            {
+                this.logger.warn("The temporary directory system property (java.io.tmpdir) did not find the system" +
+                		"temporary directory. Going to use the current Rig Client working directory.");
+                this.workingDirBase = System.getProperty("user.dir");
+                this.logger.warn("Using Rig Client working directory " + this.workingDirBase + " as batch control working " +
+                		"directory base.");
+            }
+        }
+        /* Add a new folder with a timestamp as the name. */
         
         
         return false;
@@ -165,4 +206,15 @@ public abstract class AbstractBatchRunner implements Runnable
      * @return true if successfully completed
      */
     protected abstract boolean sync();
+    
+    /**
+     * Returns <code>true</code> if a batch process has been started and is
+     * currently running. 
+     * 
+     * @return true if batch is running, false otherwise
+     */
+    public boolean isRunning()
+    {
+        return this.isRunning;
+    }
 }
