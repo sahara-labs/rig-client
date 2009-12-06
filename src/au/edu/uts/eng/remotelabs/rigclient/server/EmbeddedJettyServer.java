7403508eadb8f81f0632b58505f4fe825e5ff10b
mdiponio <mdiponio@7ee224fa-57ca-4419-87f9-1f4314938d77>
2009-12-06 23:03:02 +0000

    More embedded server impl.
    git-svn-id: https://develop.eng.uts.edu.au/projects/personal/sahara/svn/RigClient/trunk@81 7ee224fa-57ca-4419-87f9-1f4314938d77

--- src/au/edu/uts/eng/remotelabs/rigclient/server/EmbeddedJettyServer.java
+++ src/au/edu/uts/eng/remotelabs/rigclient/server/EmbeddedJettyServer.java
@@ -41,9 +41,13 @@
  */
 package au.edu.uts.eng.remotelabs.rigclient.server;
 
+import org.apache.axis2.transport.http.AxisServlet;
 import org.mortbay.jetty.Connector;
 import org.mortbay.jetty.Server;
 import org.mortbay.jetty.nio.SelectChannelConnector;
+import org.mortbay.jetty.servlet.Context;
+import org.mortbay.jetty.servlet.ServletHolder;
+import org.mortbay.thread.QueuedThreadPool;
 
 import au.edu.uts.eng.remotelabs.rigclient.util.ConfigFactory;
 import au.edu.uts.eng.remotelabs.rigclient.util.IConfig;
@@ -51,16 +55,21 @@ import au.edu.uts.eng.remotelabs.rigclient.util.ILogger;
 import au.edu.uts.eng.remotelabs.rigclient.util.LoggerFactory;
 
 /**
- * Embedded Jetty Server set up to handle SOAP requests using the Apache Axis
- * 2 
+ * Embedded Jetty Server set up to handle SOAP requests.
  */
 public class EmbeddedJettyServer implements IServer
 {
     /** Jetty server. */
     private Server server;
     
-    /** Connector which receives and handles requests.. */
+    /** Connector which receives requests. */
     private Connector connector;
+    
+    /** Context which handles a request using a servlet. */
+    private Context context;
+    
+    /** Request thread pool. */
+    private QueuedThreadPool threadPool;
     
     /** Configuration. */
     private final IConfig config;
@@ -68,13 +77,16 @@ public class EmbeddedJettyServer implements IServer
     /** Logger. */
     private final ILogger logger;
     
+    /**
+     * Constructor.
+     */
     public EmbeddedJettyServer()
     {
         this.logger = LoggerFactory.getLoggerInstance();
         this.logger.debug("Creating a new embedded Jetty server.");
                         
         this.config = ConfigFactory.getInstance();        
-        
+
     }
     
     
@@ -97,9 +109,8 @@ public class EmbeddedJettyServer implements IServer
         /* --------------------------------------------------------------------
          * ---- 2. Create and configure the connector. ------------------------
          * ----------------------------------------------------------------- */
-        /*
-         * 
-         */
+        /* The connector receives requests and calls handle on handler object
+         * to handle a request. */
         try
         {
             final int portNumber = Integer.parseInt(this.config.getProperty("Listening_Port"));
@@ -109,6 +120,7 @@ public class EmbeddedJettyServer implements IServer
                 		"configuration property 'Listening_Port' has a valid port number set.");
                 throw new ServerException("Invalid port number configuration.");
             }
+            this.logger.info("Rig client request listening port is " + portNumber + ".");
             
             // DODGY The maxIdleTimeout may need to set on the connector in case long
             // requests are truncated.
@@ -124,6 +136,35 @@ public class EmbeddedJettyServer implements IServer
         // TODO Add a HTTPS connector (SslSelectSocketConnector)
         this.server.setConnectors(new Connector[]{this.connector});
         
+        /* --------------------------------------------------------------------
+         * ---- 3. Create and configure the request thread pool. -------------- 
+         * ----------------------------------------------------------------- */
+        try
+        {
+            final int concurrentRequests = Integer.parseInt(this.config.getProperty("Concurrent_Requests"));
+            if (concurrentRequests < 1)
+            {
+                this.logger.fatal("Invalid number of allowable concurrent requests. This must be greater then 1." +
+                		" Check the configuration property 'Concurrent_Requests' has a valid number of concurrent" +
+                		" requests.");
+                throw new ServerException("Unable to load allowable concurrent request number.");
+            }
+            this.logger.info("Allowable concurrent requests is " + concurrentRequests + ".");
+            this.threadPool = new QueuedThreadPool(concurrentRequests);
+        }
+        catch (NumberFormatException e)
+        {
+            this.logger.fatal("Failed to load the number of allowable concurrent requests. Check the configuration" +
+            		" property 'Concurrent_Requests' hads a valid number of concurrent requests.");
+            throw new ServerException("Unable to load allowable concurrent request number.");
+        }
+        this.server.setThreadPool(this.threadPool);
+        
+        /* --------------------------------------------------------------------
+         * --- 4. Create and configure the handler. ---------------------------
+         * ------------------------------------------------------------------*/
+        this.context = new Context(this.server, "/", Context.SESSIONS);
+        ServletHolder holder = new ServletHolder(new AxisServlet());
         
     }
 
