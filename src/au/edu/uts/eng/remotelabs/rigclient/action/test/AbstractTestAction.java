6a53c7f1f3c28e1a27e621e3a801d52b7d805225
mdiponio <mdiponio@7ee224fa-57ca-4419-87f9-1f4314938d77>
2010-01-19 07:13:28 +0000

    Finished implementing the AbstractTestAction.
    git-svn-id: https://develop.eng.uts.edu.au/projects/personal/sahara/svn/RigClient/trunk@206 7ee224fa-57ca-4419-87f9-1f4314938d77

--- src/au/edu/uts/eng/remotelabs/rigclient/action/test/AbstractTestAction.java
+++ src/au/edu/uts/eng/remotelabs/rigclient/action/test/AbstractTestAction.java
@@ -41,6 +41,8 @@
  */
 package au.edu.uts.eng.remotelabs.rigclient.action.test;
 
+import java.util.Random;
+
 import au.edu.uts.eng.remotelabs.rigclient.rig.ITestAction;
 import au.edu.uts.eng.remotelabs.rigclient.util.ConfigFactory;
 import au.edu.uts.eng.remotelabs.rigclient.util.IConfig;
@@ -50,17 +52,26 @@ import au.edu.uts.eng.remotelabs.rigclient.util.LoggerFactory;
 /**
  * Abstract test action class which uses a run flag to implement the 
  * <code>ITestAction.run</code> contract which specifies a persistent
- * thread pausing   
+ * thread never terminating for the life of the rig client. The following
+ * flags may be modified to change its behavior:   
  * <ul>	
- *  <li>The between test pause is a random time between 0 seconds and the
- *  set run interval seconds.</li>
- *  <li>The set run interval may be set to be honoured </li>
- *  <li> 
+ *  <li><code>isPeriodic</code> - Specifies if the test interval is 
+ *  periodic (<code>true</code>), so runs every <code>runInterval</code> 
+ *  seconds or if it runs at a random time between 0 seconds and the 
+ *  <code>runInterval</code> seconds (<code>false</code>). Generally 
+ *  tests which result in <em>visible</em> effects (i.e. something moves) 
+ *  should not be periodic to mimic real lab use (not everything lab
+ *  runs at the same time). The default is for tests to be periodic. </li>
+ *  <li><code>isSetIntervalHonoured</code> - Specifies if the test 
+ *  interval is changed when the <code>setInterval</code> method is 
+ *  invoked. The default is to honour the test interval.</li>
+ *  <li><code>runInterval - The between each test run in seconds. The 
+ *  default is 60 seconds.</li>
  * </ul>
- * Abstract test action which uses a run flag to implement the test action 
- * <code>run</code> contract of a persistent thread pausing when the
- * <code>stopTest</code> method is called. The between test pause is a 
- * random interval 0 seconds and the maximum interval time. 
+ * <strong>NOTE:</strong> The abstract methods declared in this class
+ * do provide any indication of the success of their invocation. The 
+ * relative success or failure of them should be indicated by calls
+ * to <code>getStatus</code> and <code>getReason</code>.
  */
 public abstract class AbstractTestAction implements ITestAction
 {
@@ -68,12 +79,18 @@ public abstract class AbstractTestAction implements ITestAction
     protected boolean runTest;
     
     /** The maximum amount of seconds between test runs. */
-    protected int runInterval = 30;
+    protected int runInterval = 60;
+    
+    /** Whether the test is periodic or aperiodic. */
+    protected final boolean isPeriodic = true;
     
     /** Whether a call to setInterval will actually cause the test run 
      *  interval to be changed (i.e. if the specified test interval is 
      *  honoured). The default is to honour the specified test interval. */
-    protected final boolean honourSetInterval = true;
+    protected final boolean isSetIntervalHonoured = true;
+    
+    /** Random number generator. */
+    protected final Random randomNumGen;
     
     /** Logger. */
     protected final ILogger logger;
@@ -85,22 +102,86 @@ public abstract class AbstractTestAction implements ITestAction
     {
         this.logger = LoggerFactory.getLoggerInstance();
         this.config = ConfigFactory.getInstance();
+        
+        this.randomNumGen = new Random();
     }
     
     @Override
     public void run()
     {
-        while (!Thread.interrupted())
+        int sleepCount;
+        
+        /* Run setup. */
+        this.setUp();
+        
+        try
         {
-            
+            while (!Thread.interrupted())
+            {
+                /* Instead of one long sleep, this is a series of short sleeps, so 
+                 * if the test run interval is shortened past the existing sleep interval,
+                 * the existing sleep time is reduced to within the new test run
+                 * interval. */
+                if (this.isPeriodic)
+                {
+                    sleepCount = this.runInterval;
+                }
+                else
+                {
+                    sleepCount = this.randomNumGen.nextInt(this.runInterval + 1);
+                }
+                while (sleepCount > 0)
+                {
+                    if (this.runInterval < sleepCount) // Occurs when the test interval time is reduced to 
+                    {                                  // by a call to setInterval.
+                        sleepCount = this.randomNumGen.nextInt(this.runInterval + 1);
+                    }
+                    Thread.sleep(1000);
+                    sleepCount--;
+                }
+                
+                if (this.runTest)
+                {
+                    this.doTest();
+                }
+            }
+        }
+        catch (InterruptedException ex)
+        {
+            this.logger.info("Exerciser test " + this.getActionType() + " interrupted, shutting down.");
         }
         
+        this.tearDown();
     }
+    
+    /**
+     * Setup method that runs once before the first execution of the 
+     * <code>doTest</code> method.
+     */
+    public abstract void setUp();
+    
+    /**
+     * This method is the actual test method. This method is invoked at each 
+     * test interval when in run test mode. Whatever occurs within this method,
+     * calls to <code>getStatus</code> and <code>getReason</code> should
+     * reflect the results of this method.
+     * <p />
+     * If this method is long in duration (more than a few seconds), the 
+     * <code>runTest</code> flag should be regularly polled to ensure the test
+     * stops when it is instructed to stop (so when the flag is set to false,
+     * this method should return).
+     */
+    public abstract void doTest();
+    
+    /**
+     * Cleanup method that is called when the test is shutdown.
+     */
+    public abstract void tearDown();
     
     @Override
     public void setInterval(int interval)
     {
-        if (this.honourSetInterval)
+        if (this.isSetIntervalHonoured)
         {
             this.logger.info("Changing the test interval for " + this.getActionType() + " to " + interval + ".");
             this.runInterval = interval;
