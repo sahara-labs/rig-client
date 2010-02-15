f11f2a04ad87547e0c1ef2d808287625ee80e5bc
tmachet <tmachet@7ee224fa-57ca-4419-87f9-1f4314938d77>
2010-02-15 01:20:45 +0000

    git-svn-id: https://develop.eng.uts.edu.au/projects/personal/sahara/svn/RigClient/trunk@306 7ee224fa-57ca-4419-87f9-1f4314938d77

---
+++ src/au/edu/uts/eng/remotelabs/rigclient/action/access/tests/RemoteDesktopAccessActionTester.java
@@ -0,0 +1,184 @@
+/**
+ * SAHARA Rig Client
+ * 
+ * Software abstraction of physical rig to provide rig session control
+ * and rig device control. Automatically tests rig hardware and reports
+ * the rig status to ensure rig goodness.
+ *
+ * @license See LICENSE in the top level directory for complete license terms.
+ *
+ * Copyright (c) 2010, University of Technology, Sydney
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice, 
+ *    this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright 
+ *    notice, this list of conditions and the following disclaimer in the 
+ *    documentation and/or other materials provided with the distribution.
+ *  * Neither the name of the University of Technology, Sydney nor the names 
+ *    of its contributors may be used to endorse or promote products derived from 
+ *    this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author Tania Machet (tmachet)
+ * @date 12th February 2010
+ *
+ * Changelog:
+ * - 12/02/2010 - tmachet - Initial file creation.
+ */
+package au.edu.uts.eng.remotelabs.rigclient.action.access.tests;
+
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.reset;
+import static org.easymock.EasyMock.verify;
+import static org.junit.Assert.fail;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import au.edu.uts.eng.remotelabs.rigclient.action.access.RemoteDesktopAccessAction;
+import au.edu.uts.eng.remotelabs.rigclient.action.test.AbstractTestAction;
+import au.edu.uts.eng.remotelabs.rigclient.action.test.PingTestAction;
+import au.edu.uts.eng.remotelabs.rigclient.util.ConfigFactory;
+import au.edu.uts.eng.remotelabs.rigclient.util.IConfig;
+import au.edu.uts.eng.remotelabs.rigclient.util.LoggerFactory;
+
+/**
+ * Test class for testing Remote Desktop Access action
+ * 
+ * @author tmachet
+ *
+ */
+public class RemoteDesktopAccessActionTester
+{
+    /** Object of class under test. */
+    private RemoteDesktopAccessAction action;
+    
+    /** Mock configuration class. */
+    private IConfig mockConfig;
+    
+    
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception
+    {
+        this.mockConfig = createMock(IConfig.class);
+        expect(this.mockConfig.getProperty("Logger_Type"))
+                .andReturn("SystemErr");
+        expect(this.mockConfig.getProperty("Log_Level"))
+                .andReturn("DEBUG");
+        expect(this.mockConfig.getProperty("Default_Log_Format", "[__LEVEL__] - [__ISO8601__] - __MESSAGE__"))
+                .andReturn("[__LEVEL__] - [__ISO8601__] - __MESSAGE__");
+        expect(this.mockConfig.getProperty("FATAL_Log_Format")).andReturn(null);
+        expect(this.mockConfig.getProperty("PRIORITY_Log_Format")).andReturn(null);
+        expect(this.mockConfig.getProperty("ERROR_Log_Format")).andReturn(null);
+        expect(this.mockConfig.getProperty("WARN_Log_Format")).andReturn(null);
+        expect(this.mockConfig.getProperty("INFO_Log_Format")).andReturn(null);
+        expect(this.mockConfig.getProperty("DEBUG_Log_Format")).andReturn(null);
+        replay(this.mockConfig);
+        
+        Field configField = ConfigFactory.class.getDeclaredField("instance");
+        configField.setAccessible(true);
+        configField.set(null, this.mockConfig);
+
+        LoggerFactory.getLoggerInstance(); 
+        this.action = new RemoteDesktopAccessAction();
+        
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @After
+    public void tearDown() throws Exception
+    {
+        
+    }
+
+    /**
+     * Test method for {@link au.edu.uts.eng.remotelabs.rigclient.action.access.RemoteDesktopAccessAction#setupAccessAction()}.
+     */
+    @Test
+    public void testSetupAccessAction() throws Exception
+    {
+    }
+
+    /**
+     * Test method for {@link au.edu.uts.eng.remotelabs.rigclient.action.access.RemoteDesktopAccessAction#verifyAccessAction()}.
+     */
+    @Test
+    public void testVerifyAccessAction()
+    {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link au.edu.uts.eng.remotelabs.rigclient.action.access.RemoteDesktopAccessAction#RemoteDesktopAccessAction()}.
+     */
+    @Test
+    public void testRemoteDesktopAccessAction()
+    {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link au.edu.uts.eng.remotelabs.rigclient.action.access.RemoteDesktopAccessAction#assign(java.lang.String)}.
+     */
+    @Test
+    public void testAssign()
+    {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link au.edu.uts.eng.remotelabs.rigclient.action.access.RemoteDesktopAccessAction#revoke(java.lang.String)}.
+     */
+    @Test
+    public void testRevoke()
+    {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link au.edu.uts.eng.remotelabs.rigclient.action.access.RemoteDesktopAccessAction#getFailureReason()}.
+     */
+    @Test
+    public void testGetFailureReason()
+    {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link au.edu.uts.eng.remotelabs.rigclient.action.access.RemoteDesktopAccessAction#getActionType()}.
+     */
+    @Test
+    public void testGetActionType()
+    {
+        fail("Not yet implemented");
+    }
+
+}
