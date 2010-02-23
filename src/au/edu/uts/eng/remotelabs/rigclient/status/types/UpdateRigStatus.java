0fd0299651e66c0904f0e9c4f16e8ab9d59dd832
mdiponio <mdiponio@7ee224fa-57ca-4419-87f9-1f4314938d77>
2010-02-23 00:50:50 +0000

    Added scheduling server local rig provider client side SOAP interface.
    git-svn-id: https://develop.eng.uts.edu.au/projects/personal/sahara/svn/RigClient/trunk@358 7ee224fa-57ca-4419-87f9-1f4314938d77

---
+++ src/au/edu/uts/eng/remotelabs/rigclient/status/types/UpdateRigStatus.java
@@ -0,0 +1,165 @@
+/**
+ * SAHARA Rig Client
+ * 
+ * Software abstraction of physical rig to provide rig session control
+ * and rig device control. Automatically tests rig hardware and reports
+ * the rig status to ensure rig goodness.
+ *
+ * @license See LICENSE in the top level directory for complete license terms.
+ *
+ * Copyright (c) 2009, University of Technology, Sydney
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice, 
+ *    this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright 
+ *    notice, this list of conditions and the following disclaimer in the 
+ *    documentation and/or other materials provided with the distribution.
+ *  * Neither the name of the University of Technology, Sydney nor the names 
+ *    of its contributors may be used to endorse or promote products derived from 
+ *    this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author Michael Diponio (mdiponio)
+ * @date 23th February 2009
+ */
+
+package au.edu.uts.eng.remotelabs.rigclient.status.types;
+
+import javax.xml.namespace.QName;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+
+import org.apache.axiom.om.OMConstants;
+import org.apache.axiom.om.OMDataSource;
+import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.OMFactory;
+import org.apache.axiom.om.impl.llom.OMSourcedElementImpl;
+import org.apache.axis2.databinding.ADBBean;
+import org.apache.axis2.databinding.ADBDataSource;
+import org.apache.axis2.databinding.ADBException;
+import org.apache.axis2.databinding.utils.writer.MTOMAwareXMLStreamWriter;
+
+/**
+ * UpdateRigStatus bean class.
+ */
+public class UpdateRigStatus implements ADBBean
+{
+    private static final long serialVersionUID = -6089516688756716289L;
+    
+    public static final QName MY_QNAME = new QName("http://remotelabs.eng.uts.edu.au/schedserver/localrigprovider",
+            "updateRigStatus", "ns1");
+    
+    protected UpdateRigType updateRigStatus;
+
+    public UpdateRigType getUpdateRigStatus()
+    {
+        return this.updateRigStatus;
+    }
+
+    public void setUpdateRigStatus(final UpdateRigType param)
+    {
+        this.updateRigStatus = param;
+    }
+
+    public static boolean isReaderMTOMAware(final XMLStreamReader reader)
+    {
+        boolean isReaderMTOMAware = false;
+        try
+        {
+            isReaderMTOMAware = Boolean.TRUE.equals(reader.getProperty(OMConstants.IS_DATA_HANDLERS_AWARE));
+        }
+        catch (final IllegalArgumentException e)
+        {
+            isReaderMTOMAware = false;
+        }
+        return isReaderMTOMAware;
+    }
+
+    public OMElement getOMElement(final QName parentQName, final OMFactory factory) throws ADBException
+    {
+        final OMDataSource dataSource = new ADBDataSource(this, UpdateRigStatus.MY_QNAME)
+        {
+            @Override
+            public void serialize(final MTOMAwareXMLStreamWriter xmlWriter) throws XMLStreamException
+            {
+                UpdateRigStatus.this.serialize(UpdateRigStatus.MY_QNAME, factory, xmlWriter);
+            }
+        };
+        return new OMSourcedElementImpl(UpdateRigStatus.MY_QNAME, factory, dataSource);
+    }
+
+    public void serialize(final QName parentQName, final OMFactory factory, final MTOMAwareXMLStreamWriter xmlWriter)
+            throws XMLStreamException, ADBException
+    {
+        this.serialize(parentQName, factory, xmlWriter, false);
+    }
+
+    public void serialize(final QName parentQName, final OMFactory factory, final MTOMAwareXMLStreamWriter xmlWriter,
+            final boolean serializeType) throws XMLStreamException, ADBException
+    {
+        if (this.updateRigStatus == null)
+        {
+            throw new ADBException("Property cannot be null!");
+        }
+        this.updateRigStatus.serialize(UpdateRigStatus.MY_QNAME, factory, xmlWriter);
+    }
+
+    public XMLStreamReader getPullParser(final QName qName) throws ADBException
+    {
+        return this.updateRigStatus.getPullParser(UpdateRigStatus.MY_QNAME);
+    }
+
+    public static class Factory
+    {
+        public static UpdateRigStatus parse(final XMLStreamReader reader) throws Exception
+        {
+            final UpdateRigStatus object = new UpdateRigStatus();
+            try
+            {
+                while (!reader.isStartElement() && !reader.isEndElement())
+                {
+                    reader.next();
+                }
+
+                while (!reader.isEndElement())
+                {
+                    if (reader.isStartElement())
+                    {
+                        if (reader.isStartElement() && new QName("http://remotelabs.eng.uts.edu.au/schedserver/localrigprovider",
+                                "updateRigStatus").equals(reader.getName()))
+                        {
+                            object.setUpdateRigStatus(UpdateRigType.Factory.parse(reader));
+                        }
+                        else
+                        {
+                            throw new ADBException("Unexpected subelement " + reader.getLocalName());
+                        }
+                    }
+                    else
+                    {
+                        reader.next();
+                    }
+                }
+            }
+            catch (final XMLStreamException e)
+            {
+                throw new Exception(e);
+            }
+            return object;
+        }
+    }
+}
