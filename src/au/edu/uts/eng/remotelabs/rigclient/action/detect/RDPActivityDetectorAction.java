c8ee241dc910f4ab03411ae3797ba1f34d8b6607
tmachet <tmachet@7ee224fa-57ca-4419-87f9-1f4314938d77>
2010-03-04 04:24:00 +0000

    small changes
    git-svn-id: https://develop.eng.uts.edu.au/projects/personal/sahara/svn/RigClient/trunk@420 7ee224fa-57ca-4419-87f9-1f4314938d77

--- src/au/edu/uts/eng/remotelabs/rigclient/action/detect/RDPActivityDetectorAction.java
+++ src/au/edu/uts/eng/remotelabs/rigclient/action/detect/RDPActivityDetectorAction.java
@@ -44,7 +44,6 @@ package au.edu.uts.eng.remotelabs.rigclient.action.detect;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.List;
 
 import au.edu.uts.eng.remotelabs.rigclient.rig.IActivityDetectorAction;
 import au.edu.uts.eng.remotelabs.rigclient.util.ILogger;
