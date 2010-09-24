b05dc14b998b315f48d8645182c6f3d199c49366
mdiponio <mdiponio@7ee224fa-57ca-4419-87f9-1f4314938d77>
2010-09-24 08:28:12 +0000

    Minor tool tip fixes.
    git-svn-id: https://develop.eng.uts.edu.au/projects/personal/sahara/svn/RigClient/trunk@1734 7ee224fa-57ca-4419-87f9-1f4314938d77

--- src/au/edu/uts/eng/remotelabs/rigclient/server/pages/IndexPage.java
+++ src/au/edu/uts/eng/remotelabs/rigclient/server/pages/IndexPage.java
@@ -89,16 +89,15 @@ public class IndexPage extends AbstractPage
         this.toolTips = new HashMap<String, String>(9);
         this.toolTips.put("Status", "The status of the rig client including session details and exerciser tests " +
         		"statuses.");
-        this.toolTips.put("Configuration", "Allows configuration properties of the rig client can be changed.");
-        this.toolTips.put("Logs", "Log viewer of the recent log messages of the rig client. Only the " + 
-                this.logger.getLogBuffer().length + " most recent log messages since the rig client was started are " +
-                "displayed");
+        this.toolTips.put("Configuration", "Allows configuration properties of the rig client to be viewed and " +
+        		"changed.");
+        this.toolTips.put("Logs", "Log viewer of the latest log messages of the rig client since startup.");
         this.toolTips.put("Documentation", "Documentation about the rig client.");
         this.toolTips.put("Runtime_Information", "Runtime information about the rig client such as classpath, system " +
         		"properties, uptime...");
-        this.toolTips.put("Restart", "Restarts the rig client. This is only a soft restart as the Java virtual " +
-        		"machine is not restarted.");
-        this.toolTips.put("Shutdown", "Shuts down the rig client. Terminates the rig client service");
+        this.toolTips.put("Restart", "Restarts the rig client. This is only a soft restart since the rig client service " +
+        		"process is not restarted.");
+        this.toolTips.put("Shutdown", "Shuts down the rig client. The rig client service is stopped.");
     }
     
     @Override
